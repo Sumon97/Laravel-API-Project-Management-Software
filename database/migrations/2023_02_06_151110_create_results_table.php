ee54adc3fb3fe54f0b28eb73d317c1dbdf64448d
Muhammad Sumon Ahmed <96331936+Sumon97@users.noreply.github.com>
2023-02-06 21:53:53 +0600

    Update 2023_02_06_151110_create_results_table.php

--- database/migrations/2023_02_06_151110_create_results_table.php
+++ database/migrations/2023_02_06_151110_create_results_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         Schema::create('results', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
             $table->timestamps();
         });
     }
